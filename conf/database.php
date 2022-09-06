86c9cd07075a3f10748f61b0e181d0ac5a64bc82
nuclearzzet <nikettjohn@outlook.com>
2022-09-06 18:22:39 +0530

    🎨 Improved code and structure

---
+++ conf/database.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'nuclear');
+define('DB_PASS', 'nuclearzzet*779');
+define('DB_NAME', 'personal_website');
+
+
---
+++ inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuclearzzet</title>
+    <link rel="stylesheet" href="../css/navbar.css">
+</head>
+<header>
+    <div id="flex-header">
+            <h1 id="heading">nuclearzzet.dev</h1>
+            <ul id="nav__bar">
+                <li class="nav__link"><a href="/personal_website/">Home</a></li>
+                <li class="nav__link"><a href="https://github.com/nuclearzzet" target="_blank">Github</a></li>
+                <li class="nav__link"><a href="/personal_website/pages/projects.php">Projects</a></li>
+            </ul>
+            <a href="./pages/contactMe.html"><button>Contact Me</button></a>
+    </div>
+</header>
