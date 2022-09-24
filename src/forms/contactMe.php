63460c58dd2c6fea8f5062b03e06e69ec364e579
nuclearzzet <nikettjohn@outlook.com>
2022-09-24 15:42:22 +0530

    ✨ Minor Corrections

--- src/forms/contactMe.php
+++ src/forms/contactMe.php
@@ -4,14 +4,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
     $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_SPECIAL_CHARS);
 
     if (!empty($email)){
         if (!empty($name)){
-            if (!empty($body)){
-                header('LOCATION: /personal_website/index');
+            if (!empty($title)){
+                if (!empty($body)){
+                    header('LOCATION: /personal_website/index');
+                }else{
+                    $message =  "Message cannot be empty";
+                }
             }else{
-                $message =  "Message cannot be empty";
+                $message = "Title cannot be empty";
             }
         }else{
             $message =  "Name cannot be empty";
