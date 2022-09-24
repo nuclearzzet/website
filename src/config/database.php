d6b80a4937c8f8461f24fbe1e7a9583b0a2e32f7
nuclearzzet <nikettjohn@outlook.com>
2022-09-24 15:39:06 +0530

    ✨ Database and Models

--- src/config/database.php
+++ src/config/database.php
@@ -1,3 +1,42 @@
 <?php
 
-echo "Connected";
+class Database{
+    
+    private $host = 'localhost';
+    private $user = 'personal_website';
+    private $password = 'password*123';
+    private $name = 'personal_website';
+
+    private $conn;
+
+    public function connect(){
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO('mysql:host= ' . $this->host . ';dbname= ' . $this->name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $err){
+            echo "Connection Failed: " . $err->getMessage();
+        }
+
+        return $this->conn;
+    }
+
+    // Getters
+
+    public function getHost(){
+        return $this->host;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getUser(){
+        return $this->user;
+    }
+
+    public function getPassword(){
+        return $this->password;
+    }
+}
