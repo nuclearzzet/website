d1192740fd418931edee0c8f4b14173711ac0c62
nuclearzzet <nikettjohn@outlook.com>
2022-09-28 22:12:49 +0530

    🎨 More Formatting

--- src/config/database.php
+++ src/config/database.php
@@ -1,33 +1,16 @@
 <?php
 
 class Database{
-    
-    
-    private $host;
-    private $user;
-    private $pass;
-    private $name;
-
-    private $conn;
 
     public static function connect(){
-        try{
 
-            $host = 'localhost';
-            $user = 'personal_website';
-            $pass = 'personal_website*779';
-            $name = 'personal_website';
+        $host = 'localhost';
+        $user = 'personal_website';
+        $pass = 'personal_website*779';        
+        $name = 'personal_website';
 
-            $conn = new mysqli($host, $user, $pass, $name);
+        $conn = new mysqli($host, $user, $pass, $name);
 
-            if($conn){
-                return $conn;
-            }else{
-                $message = 'Could not Connection to db';
-                throw new Exception($message);
-            }
-        }catch(error_get_last $err){
-            echo "Connection Failed: " . $err->getMessage();
-        }
+        return $conn;
     }
 }
